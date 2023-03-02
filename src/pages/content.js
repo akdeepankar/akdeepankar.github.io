24f9046e5bec0ca40ba0411763ebce5825206fa3
AK DEEPANKAR <akdeepaknyc@gmail.com>
2023-03-02 14:00:04 +0530

    Dynamic

---
+++ src/pages/content.js
@@ -0,0 +1,83 @@
+import {TiSocialLinkedin, TiSocialTwitter, TiSocialYoutube} from 'react-icons/ti'
+import {BsBehance, BsGithub} from 'react-icons/bs'
+import {AiFillInstagram} from 'react-icons/ai'
+
+
+export const intro = {
+    introlottie: 'https://assets7.lottiefiles.com/packages/lf20_aesgckiv.json',
+    greet: 'Hey There!',
+    intro: 'I am',
+    name: 'AK Deepankar.',
+    desc: 'Student, who is either busy ',
+    desc2: 'Experimenting',
+    desc3: 'or',
+    desc4: 'Pondering',
+    desc5: 'over the',
+    desc6: 'Next Big Idea.',
+    skilllottie: 'https://assets1.lottiefiles.com/packages/lf20_ht8zw3es.json',
+    projectlottie: 'https://assets2.lottiefiles.com/packages/lf20_hylaaytn.json',
+}
+
+export const a = ['ps.svg', 'illustrator.svg', 'fresco.svg', 'xd.svg', 'lightroom.svg', 'animate.svg'];
+export const flat = ['HTML', 'CSS', 'JavaScript']
+export const primary = ['BootStrap', 'Tailwind CSS', 'React', 'NextJs']
+export const secondary = ['Flask', 'Django']
+export const success = ['C', 'Python', 'Java']
+export const warning = ['Matplotlib']
+export const error = ['VS Code', 'Github']
+
+
+export const social = [
+    {
+      id: "social-1",
+      icon: TiSocialLinkedin,
+      link: "https://linkedin.com/in/akdeepankar/",
+    },
+    {
+      id: "social-2",
+      icon: TiSocialTwitter,
+      link: "https://twitter.com/@ak_deepankar/",
+    },
+    {
+      id: "social-3",
+      icon: TiSocialYoutube,
+      link: "https://www.youtube.com/@ak_deepankar",
+    },
+    {
+      id: "social-4",
+      icon: AiFillInstagram,
+      link: "https://instagram.com/ak_deepankar/",
+    },
+    {
+      id: "social-5",
+      icon: BsBehance,
+      link: "https://behance.net/akdeepankar/",
+    },
+    {
+        id: "social-6",
+        icon: BsGithub,
+        link: "https://github.com/akdeepankar/",
+    },
+  ];
+
+
+  export const project = [
+    {
+        id: "p-1",
+        background: 'https://images.pexels.com/photos/811587/pexels-photo-811587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        head1: "New",
+        head2: "PORTFOLIO",
+        foot1: "Currently Viewing",
+        foot2: "Portfolio.",
+        techstack: "NEXT JS, TAILWIND CSS",
+    },
+  ];
+
+  
+export const poster = [
+    {
+        background: 'https://nextui.org/images/card-example-4.jpeg',
+        head1: 'Teaming Up For A Project or Hackathon ?',
+        head2: 'Just Ping Me! Always Open to New ideas.',
+    },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,12 @@
 import Head from 'next/head'
-import {BsFillMoonStarsFill} from 'react-icons/bs'
-import {TiSocialLinkedin, TiSocialTwitter, TiSocialYoutube, TiSocialInstagram, TiSocialGithub} from 'react-icons/ti'
-import {BsBehance, BsGithub} from 'react-icons/bs'
-import {AiFillInstagram} from 'react-icons/ai'
-import { Card, Row, Button } from "@nextui-org/react";
-import { Collapse, Col, Text, Grid, Badge, Avatar, Link, Switch } from "@nextui-org/react";
+import { Card, Row, Button, Col, Text, Grid, Badge, Avatar, Switch } from "@nextui-org/react";
 import { SunIcon } from './SunIcon';
 import { MoonIcon } from './MoonIcon';
 import { useState } from 'react'
-
-const a = ['ps.svg', 'illustrator.svg', 'fresco.svg', 'xd.svg', 'lightroom.svg', 'animate.svg'];
-const flat = ['HTML', 'CSS', 'JavaScript']
-const primary = ['BootStrap', 'Tailwind CSS', 'React', 'NextJs']
-const secondary = ['Flask', 'Django']
-const success = ['C', 'Python', 'Java']
-const warning = ['Matplotlib']
-const error = ['VS Code', 'Github']
+import {a, flat, primary, secondary, success, warning, error, social, project, poster, intro} from './content'
 
 
-
-export default function Home(props) {
+export default function Home() {
   const [darkMode, setDarkMode] = useState(false)
   return (
     <div className={darkMode ? "dark" : ""} >
@@ -46,10 +33,10 @@ export default function Home(props) {
           </nav>  
           <div className='flex justify-center -mt-24'>
           <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-          <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_aesgckiv.json"  background="transparent" speed="1"  style={{width: 500, height: 500}}  loop  autoplay></lottie-player>
+          <lottie-player src={intro.introlottie}  background="transparent" speed="1"  style={{width: 500, height: 500}}  loop  autoplay></lottie-player>
           </div>
           <div className='text-center -mt-20' >
-            <h3 className='text-1xl md:text-4xl dark:text-white'>Hey There!</h3>
+            <h3 className='text-1xl md:text-4xl dark:text-white'>{intro.greet}</h3>
             <div className='flex justify-center '>
               <Text
               className='dark:text-white'
@@ -58,7 +45,7 @@ export default function Home(props) {
               size={30}
               weight="bold"
                >
-               I am
+               {intro.intro}
             </Text>
             <Text
               className='ml-3'
@@ -69,10 +56,10 @@ export default function Home(props) {
               }}
               weight="bold"
                >
-               AK Deepankar.
+               {intro.name}
             </Text>
             </div>
-            <h3 className='text-gray-800 md:text-2xl md:pt-5 max-w-xl mx-auto dark:text-white'>Student, who is either busy <em>Experimenting</em>  <br></br> or <em>Pondering</em> over the <em className='bg-green-100 dark:text-black'>Next Big Idea.</em></h3>
+            <h3 className='text-gray-800 md:text-2xl md:pt-5 max-w-xl mx-auto dark:text-white'>{intro.desc}<em>{intro.desc2}</em>  <br></br> {intro.desc3} <em>{intro.desc4}</em> {intro.desc5} <em className='bg-green-100 dark:text-black'>{intro.desc6}</em></h3>
           </div>
 
           <div className='flex justify-center pt-5 '>
@@ -84,15 +71,13 @@ export default function Home(props) {
         zIndex: 1,
       }} >
            <Card.Body>
-            <Row className='flex justify-between' >
-          <a href='https://linkedin.com/in/akdeepankar/'><TiSocialLinkedin className='cursor-pointer text-2xl ml-5 dark:text-white'/></a>
-          <a href="https://twitter.com/@ak_deepankar/"><TiSocialTwitter className='cursor-pointer text-2xl ml-5 dark:text-white'/></a> 
-          <a href="https://www.youtube.com/@ak_deepankar"><TiSocialYoutube className='cursor-pointer text-2xl ml-5 dark:text-white'/></a>
-          <a href="https://instagram.com/ak_deepankar/"><AiFillInstagram className='cursor-pointer text-2xl ml-5 dark:text-white'/></a> 
-          <a href="https://behance.net/akdeepankar/"><BsBehance className='cursor-pointer text-2xl ml-5 dark:text-white'/></a> 
-          <a href="https://github.com/akdeepankar/"><BsGithub className='cursor-pointer text-2xl ml-5 mr-5 dark:text-white'/></a>    
-            </Row>
-           
+            <Row className='flex justify-between' >    
+             {social.map(i => {
+                return(
+                  <a href={i.link}><i.icon className='cursor-pointer text-2xl ml-5 dark:text-white'/></a>
+                )  
+                })}
+           </Row>        
            </Card.Body>
           </Card>
           </Grid>
@@ -115,7 +100,7 @@ export default function Home(props) {
                 <h1 className='text-3xl text-black-600 pt-5 font-bold -mt-4 dark:text-white '>Skills</h1>
                 </div>
                 <div className='-mt-2 ml-2'>
-              <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_ht8zw3es.json"  background="transparent"  speed="1"  style={{width: 50, height: 50}}  loop  autoplay></lottie-player>
+              <lottie-player src={intro.skilllottie}  background="transparent"  speed="1"  style={{width: 50, height: 50}}  loop  autoplay></lottie-player>
               </div>
               </Card.Header>
               <Card.Divider className='dark:bg-white' />
@@ -199,99 +184,106 @@ export default function Home(props) {
                 <h1 className='text-3xl text-black-600 pt-5 font-bold -mt-4 dark:text-white '>Projects</h1>
                 </div>
                 <div className='-mt-2 ml-2'>
-              <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_hylaaytn.json"  background="transparent"  speed="1"  style={{width: 50, height: 50}}  loop  autoplay></lottie-player>
+              <lottie-player src={intro.projectlottie}  background="transparent"  speed="1"  style={{width: 50, height: 50}}  loop  autoplay></lottie-player>
               </div>
               </Card.Header>
           <Card.Divider className='dark:bg-white' />
           <Card.Body css={{ py: "$10" }}>
             <div>
-            <Card css={{ w: "100%", h: "200px" }}>
-    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-      <Col>
-        <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-          New
-        </Text>
-        <Text h3 color="white">
-          PORTFOLIO
-        </Text>
-      </Col>
-    </Card.Header>
-    <Card.Body css={{ p: 0 }}>
-      <Card.Image
-        src="https://images.pexels.com/photos/811587/pexels-photo-811587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        width="100%"
-        height="100%"
-        objectFit="cover"
-        alt="Card example background"
-      />
-    </Card.Body>
-    <Card.Footer
-      isBlurred
-      css={{
-        position: "absolute",
-        bgBlur: "#ffffff66",
-        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
-        bottom: 0,
-        zIndex: 1,
-      }}
-    >
-      <Row>
-        <Col>
-          <Text color="#000" size={12}>
-            Currently Viewing
-          </Text>
-         <Text color="#000" size={12}>
-          Portfolio. 
-          </Text> 
-        </Col>
-        <Col>
-          <Row justify="flex-end">
-            <Button flat auto rounded color="white">
-              <Text
-                css={{ color: "black" }}
-                size={12}
-                weight="bold"
-                transform="uppercase"
-              >
-                NEXT Js, Tailwind CSS
-              </Text>
-            </Button>
-          </Row>
-        </Col>
-      </Row>
-    </Card.Footer>
-  </Card>
+
+            {project.map(i => {
+            return <Card css={{ w: "100%", h: "200px" }} className='mb-2'>
+            <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+              <Col>
+                <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+                     {i.head1}
+                </Text>
+                <Text h3 color="white">
+                    {i.head2}
+                </Text>
+              </Col>
+            </Card.Header>
+                <Card.Body css={{ p: 0 }}>
+                    <Card.Image
+                    src={i.background}
+                    width="100%"
+                    height="100%"
+                    objectFit="cover"
+                    alt="Card example background"
+                    />
+                </Card.Body>
+          <Card.Footer
+                    isBlurred
+                    css={{
+                      position: "absolute",
+                      bgBlur: "#ffffff66",
+                      borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                      bottom: 0,
+                      zIndex: 1,
+                    }}
+                    >
+            <Row>
+                <Col>
+                    <Text color="#000" size={12}>
+                      {i.foot1}
+                    </Text>
+                    <Text color="#000" size={12}>
+                      {i.foot2}
+                    </Text> 
+                </Col>
+              <Col>
+                <Row justify="flex-end">
+                  <Button flat auto rounded color="white">
+                    <Text
+                      css={{ color: "black" }}
+                      size={12}
+                      weight="bold"
+                      transform="uppercase"
+                    >
+                      {i.techstack}
+                    </Text>
+                  </Button>
+                </Row>
+              </Col>
+            </Row>
+          </Card.Footer>
+        </Card>
+            })}
+
             </div>
           </Card.Body>
         </Card>
       </Grid>
 
 
-      
 </section>
 
 
 
-<section className='mt-5 flex justify-center mb-5 md:text-2xl md:pt-5 max-w-xl mx-auto'>
-<Card className=' border-none h-40'>
-    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-      <Col>
-        <Text className='bg-black' size={16} weight="bold" transform="uppercase" color="#ffffff">
-          Teaming Up For A Project or Hackathon ?
-        </Text>
-        <Text className='bg-black' h4 color="white">
-          Just Ping Me! Always Open to New ideas.
-        </Text>
-      </Col>
-    </Card.Header>
-    <Card.Image
-      src="https://nextui.org/images/card-example-4.jpeg"
-      objectFit="cover"
-      width="100%"
-      height="100%"
-      alt="Card image background"
-    />
-  </Card>
+<section className='mt-5 mb-5 md:text-2xl md:pt-5 max-w-xl mx-auto'>
+
+{poster.map(i => {
+            return <Card className=' border-none h-40 mb-2'>
+            <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+              <Col>
+                <Text className='bg-black' size={16} weight="bold" transform="uppercase" color="#ffffff">
+                  {i.head1}
+                </Text>
+                <Text className='bg-black' h4 color="white">
+                  {i.head2}
+                </Text>
+              </Col>
+            </Card.Header>
+            <Card.Image
+              src={i.background}
+              objectFit="cover"
+              width="100%"
+              height="100%"
+              alt="Card image background"
+            />
+          </Card>
+            })}
+
 </section>
 <section className='flex justify-center mb-5'>
 <Button bordered color="primary" auto>
