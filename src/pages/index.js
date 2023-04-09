33de8db75537702ffde8c10a7d7e4546b14e7b49
AK DEEPANKAR <akdeepaknyc@gmail.com>
2023-04-10 02:29:20 +0530

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -235,8 +235,8 @@ export default function Home() {
               <Card.Divider className='dark:bg-white' />
               <Card.Body css={{ py: "$10" }} >
                 <div className='flex'>
-                  <Image width={50} className='rounded-xl' src="https://play-lh.googleusercontent.com/fH_k4XeB6HsbQJp3AuMjJ3PTDgeMX45mkNEZO4bGs6CoCgSnjmlHG0dzQwK6SVCU1Q=w240-h480-rw" alt=""></Image>  
-                  <div className='p-1'>
+                  <Image width={40} className='rounded-xl' src="https://play-lh.googleusercontent.com/fH_k4XeB6HsbQJp3AuMjJ3PTDgeMX45mkNEZO4bGs6CoCgSnjmlHG0dzQwK6SVCU1Q=w240-h480-rw" alt=""></Image>  
+                  <div className='p-2'>
                   <Link href={'https://play.google.com/store/apps/details?id=ak.buddhatunes'}>
                                   <Image width={100} className='rounded-xl pt-2 pb-2' src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Google_Play_2022_logo.svg" alt=""></Image>  
                 </Link>
@@ -246,7 +246,7 @@ export default function Home() {
         </Card>
         
         <div className='pt-2'>
-            <Image width={200} className='rounded-lg ' src="pandasleeping.png" alt=""></Image>  
+            <Image width={100} className='rounded-lg ' src="pandasleeping.png" alt=""></Image>  
         </div>
 
         
